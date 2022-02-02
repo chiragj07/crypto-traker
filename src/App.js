4ed02d6b4fc6e940b8801331f456fdbf4cd4de07
chiragj07 <joshi.chirag54@gmail.com>
2022-02-02 17:11:13 +0530

    search sort pagination1

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import { useEffect, useState } from 'react';
-import Home from './Page/Home/Home';
-import axios from 'axios';
-import CoinDetails from './Page/CoinDetails/CoinDetails';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import { useEffect, useState } from "react";
+import Home from "./Page/Home/Home";
+import axios from "axios";
+import CoinDetails from "./Page/CoinDetails/CoinDetails";
 function App() {
   const [loading, setLoading] = useState(true);
-      const[allCurr, setAllCurr]=useState([]);
+  const [allCurr, setAllCurr] = useState([]);
 
-  useEffect(()=>{
-      
-      const fetchAllcurr = ()=>{
-        var options = {
-          method: 'GET',
-          url: 'https://coinranking1.p.rapidapi.com/reference-currencies',
-          params: {limit: '50', offset: '0'},
-          headers: {
-            'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-            'x-rapidapi-key': '8f3db16d2amsh09a51c7d890956fp1abbb2jsndea6444c567e'
-          }
-        };
-        axios.request(options).then(({data})=>{
-        const arr = data.data.currencies.slice(0,14);
-        setAllCurr(arr);
-        setLoading(false)
-      
-        }).catch(err => console.log("somethng went wrong"+err.message));
-
-      }
-      fetchAllcurr()
-  },[])
+  useEffect(() => {
+    const fetchAllcurr = () => {
+      var options = {
+        method: "GET",
+        url: "https://coinranking1.p.rapidapi.com/reference-currencies",
+        params: { limit: "50", offset: "0" },
+        headers: {
+          "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "8f3db16d2amsh09a51c7d890956fp1abbb2jsndea6444c567e",
+        },
+      };
+      axios
+        .request(options)
+        .then(({ data }) => {
+          const arr = data.data.currencies.slice(0, 14);
+          setAllCurr(arr);
+          setLoading(false);
+        })
+        .catch((err) => console.log("somethng went wrong" + err.message));
+    };
+    fetchAllcurr();
+  }, []);
   return (
-    
     <div className="App">
-       {!loading && <Navbar allCurr={allCurr} /> }
-       <Routes >
-        <Route path={'/'} exact element={<Home />} />
-        <Route path={'coindetails/:coinId'} exact element={<CoinDetails />} />
-       </Routes>
-
-        
+      {!loading && <Navbar allCurr={allCurr} />}
+      <Routes>
+        <Route path={"/"} exact element={<Home />} />
+        <Route path={"coindetails/:coinId"} exact element={<CoinDetails />} />
+      </Routes>
     </div>
-    
   );
 }
 
--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import {Line} from 'react-chartjs-2'
-import {Chart,LineElement,PointElement,CategoryScale,LinearScale} from 'chart.js'
-import { useSelector } from 'react-redux';
-Chart.register(
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    PointElement
-)
-const ChartComponent = ({coinHistory}) => {
-    const currency= useSelector(state=>state.currency);   
-    
-    const data = {
-        labels: coinHistory.map((his) => {
-            return new Date (his.timestamp).toLocaleDateString()
-        }),
-        datasets:[
-            {
-            data: coinHistory.map(coin=> coin.price),
-            label:currency ? `price in ${currency.name}` : "price in USD",
-            backgroundColor:'gold',
-            borderColor:'gold',
-            fill:false
-            
-        }
-    ]
-    }
-    
-  return (<>
-  <Line data= {data} 
-    options={ 
-        
-        {
-            elements: {
-                point :{
-                    radius:1
-                }
-            }
-        }
-    }
-  />
-  </>);
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { useSelector } from "react-redux";
+Chart.register(CategoryScale, LinearScale, LineElement, PointElement);
+const ChartComponent = ({ coinHistory }) => {
+  const currency = useSelector((state) => state.currency);
+
+  const data = {
+    labels: coinHistory.map((hist) => {
+      return new Date(hist.timestamp).toLocaleDateString();
+    }),
+    datasets: [
+      {
+        data: coinHistory.map((coin) => coin.price),
+        label: currency ? `price in ${currency.name}` : "price in USD",
+        backgroundColor: "gold",
+        borderColor: "gold",
+        fill: false,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Line
+        data={data}
+        options={{
+          elements: {
+            point: {
+              radius: 1,
+            },
+          },
+        }}
+      />
+    </>
+  );
 };
 
 export default ChartComponent;
